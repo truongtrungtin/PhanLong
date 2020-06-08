e143ef50a078ac1b552b5b9c07e90719d782b869
Truong Trung Tin <trungtin.230599@hotmail.com>
2020-06-08 15:48:18 +0700

    a

---
+++ QLDL/Areas/ThongKe/Controllers/ChiTietController.cs
@@ -0,0 +1,20 @@
+﻿using QLDL.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLDL.Areas.ThongKe.Controllers
+{
+    public class ChiTietController : BaseController
+    {
+        // GET: ThongKe/Chitiet
+        public ActionResult Index()
+        {
+            var dao = new PhatSinhDao();
+            var model = dao.ListAll();
+            return View(model);
+        }
+    }
+}
